e77c87d01fe60a298965b1df56d7c0d8b6c06a54
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-08 09:19:25 -0800

    Got the server.js setup up next is the config file and then we can get the server up and running

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const eHandleBars = require("express-handlebars");
+const session = require("express-session");
+const path = require("path");
+const routes = require("./controller");
+const sequelize = require("./config/connection");
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+const hbs = eHandleBars.create({});
+
+const sess = {
+    secret:"SHhh its a secret",
+    cookie:{},
+    resave:false,
+    saveUninitialized:true,
+    store: new sequelizeStore({
+        db:sequelize,
+    })
+}
+
+app.use(session(sess));
+app.engine("handlebars",hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(routes);
+
+sequelize.sync({force:false}).then(() => {
+    app.listen(PORT, () => console.log("now listening") )
+});
