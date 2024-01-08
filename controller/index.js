0ba3938885f004e1145458bfb41dc2775bee772d
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-08 11:40:58 -0800

    Got my config setup and working with my server.js folder also got a basic controller folder

---
+++ controller/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const apiRoute = require("./api");
+const homeRoutes = require("./homeRoutes");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoute);
+
+module.exports = router;
