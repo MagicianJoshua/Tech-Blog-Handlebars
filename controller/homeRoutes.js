bca5b9482fd70f36ddb4368ae4142d472141979a
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-08 20:49:15 -0400

    Added a user model got some homeroutes and added a authentication util

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,20 +1,31 @@
 const router = require("express").Router();
 
 router.use((req, res, next) => {
-    res.locals.logged_in = req.session.logged_in;
-    next();
-  });
+  res.locals.logged_in = req.session.logged_in;
+  next();
+});
 
-  router.get('/', async (req, res) => {
-    // Send the rendered Handlebars.js template back as the response
-  
+router.get("/", async (req, res) => {
+  // Send the rendered Handlebars.js template back as the response
+
+  try {
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req,res) => {
+
     try {
-      res.render('homepage' , {
-          logged_in: req.session.logged_in});
-    }catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+        res.render("login")
+    } catch (err) {
+        res.status(500).json(err);
     }
-  });
 
-  module.exports = router;
+}) 
+
+module.exports = router;
