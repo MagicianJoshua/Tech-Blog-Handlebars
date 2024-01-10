c1dd87fc724b17eea3e1b44d937e0091b8948c83
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-09 21:40:49 -0400

    Final push for this project, i have implemented everything im required to do

--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -3,10 +3,29 @@ const {Post} = require("../models");
 
 const postData = [
     {
-        id:1,
         title:"Why i love sql",
         text:"Its the best ever",
         user_id:1,
+    },
+    {
+        title:"typescript",
+        text:"typescript is so good",
+        user_id:2,
+    },
+    {
+        title:"react",
+        text:"react is so much better than handlebars",
+        user_id:3,
+    },
+    {
+        title:"python",
+        text:"I really hate python because of all the whitespacing",
+        user_id:4,
+    },
+    {
+        title:"c#",
+        text:"I love c# because you have to define variable types",
+        user_id:5,
     }
 ]
 
