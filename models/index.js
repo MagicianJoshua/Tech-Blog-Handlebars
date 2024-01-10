6029c6edf54bda878dd0171565c6c39113544b66
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-10 02:11:35 -0400

    Making sure that everything is pointing directly at the index.js

--- models/index.js
+++ models/index.js
@@ -1,36 +1,36 @@
-const {User} = require("./User");
-const {Post} = require("./Post");
-const {Comment} = require("./Comment");
+const {User} = require("./User.js");
+const {Post} = require("./Post.js");
+const {Comment} = require("./Comment.js");
 
-// User.hasMany(Post, {
-//     foreignKey:"user_id"
-// })
-// Post.belongsTo(User, {
-//     foreignKey:"user_id",
-//     onDelete:"CASCADE",
-// });
-
-
-// User.hasMany(Comment, {
-//     foreignKey:"user_id",
-//     onDelete:"CASCADE"
-// })
-// Comment.belongsTo(User, {
-//     foreignKey:"user_id",
-//     onDelete:"CASCADE"
-// })
+User.hasMany(Post, {
+    foreignKey:"user_id"
+})
+Post.belongsTo(User, {
+    foreignKey:"user_id",
+    onDelete:"CASCADE",
+});
 
 
+User.hasMany(Comment, {
+    foreignKey:"user_id",
+    onDelete:"CASCADE"
+})
+Comment.belongsTo(User, {
+    foreignKey:"user_id",
+    onDelete:"CASCADE"
+})
 
-// Post.hasMany(Comment,{
-//     foreignKey:"post_id",
-//     as:"comments",
-//     onDelete:"CASCADE"
-// })
-// Comment.belongsTo(Post,{
-//     foreignKey:"post_id",
-//     as:"post",
-//     onDelete:"CASCADE",
-// })
+
+
+Post.hasMany(Comment,{
+    foreignKey:"post_id",
+    as:"comments",
+    onDelete:"CASCADE"
+})
+Comment.belongsTo(Post,{
+    foreignKey:"post_id",
+    as:"post",
+    onDelete:"CASCADE",
+})
 
 module.exports = { Post , User , Comment };
