0b89327fb217c2c43b66586e64abc0e67161d8db
Joshua Kennedy-Dedam <joshuajordyn@hotmail.com>
2024-01-10 02:06:07 -0400

    trying something

--- models/index.js
+++ models/index.js
@@ -1,36 +1,36 @@
-const {User} = require("./User");
-const {Post} = require("./Post");
-const {Comment} = require("./Comment");
+// const {User} = require("./User");
+// const {Post} = require("./Post");
+// const {Comment} = require("./Comment");
 
-User.hasMany(Post, {
-    foreignKey:"user_id"
-})
-Post.belongsTo(User, {
-    foreignKey:"user_id",
-    onDelete:"CASCADE",
-});
-
-
-User.hasMany(Comment, {
-    foreignKey:"user_id",
-    onDelete:"CASCADE"
-})
-Comment.belongsTo(User, {
-    foreignKey:"user_id",
-    onDelete:"CASCADE"
-})
+// User.hasMany(Post, {
+//     foreignKey:"user_id"
+// })
+// Post.belongsTo(User, {
+//     foreignKey:"user_id",
+//     onDelete:"CASCADE",
+// });
 
 
+// User.hasMany(Comment, {
+//     foreignKey:"user_id",
+//     onDelete:"CASCADE"
+// })
+// Comment.belongsTo(User, {
+//     foreignKey:"user_id",
+//     onDelete:"CASCADE"
+// })
 
-Post.hasMany(Comment,{
-    foreignKey:"post_id",
-    as:"comments",
-    onDelete:"CASCADE"
-})
-Comment.belongsTo(Post,{
-    foreignKey:"post_id",
-    as:"post",
-    onDelete:"CASCADE",
-})
 
-module.exports = { Post , User , Comment };
+
+// Post.hasMany(Comment,{
+//     foreignKey:"post_id",
+//     as:"comments",
+//     onDelete:"CASCADE"
+// })
+// Comment.belongsTo(Post,{
+//     foreignKey:"post_id",
+//     as:"post",
+//     onDelete:"CASCADE",
+// })
+
+// module.exports = { Post , User , Comment };
